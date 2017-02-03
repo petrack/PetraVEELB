7ffe495a535a36e14b0430cba2a9e935c0c33a3b
DESKTOP-7HV61R4\Petra <petra_kujawa>
2017-02-03 15:44:28 -0600

    Random other files

---
+++ VEELB/VEELB/FileAccess.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+namespace VEELB
+{
+	public ref class FileAccess sealed
+	{
+	public:
+		//FileAccess(Platform::String^ fileName);
+	private:
+		/*void CreateFile();
+		bool WriteTextToFile(Platform::String^ inputText);
+		Platform::String^ ReadTextFromFile();
+		Platform::String^ fileName;
+		StorageFile^ file;*/
+	};
+}
