98bb0a7905f3a5476338563c92d0df5158745b3c
DESKTOP-7HV61R4\Petra <petra_kujawa>
2017-02-03 15:41:46 -0600

    Commiting other files including asset images

---
+++ VEELB/VEELB/Console.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "Console.h"
+
+using namespace VEELB;
+
+using namespace concurrency;
+using namespace Platform;
+using namespace Windows::Storage;
+using namespace Windows::UI::Xaml;
+
+Console::Console()
+{
+
+}
---
+++ VEELB/VEELB/Console.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+namespace VEELB
+{
+	public ref class Console sealed
+	{
+	public:
+		Console();
+	};
+}
